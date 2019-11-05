41d7b840397a1e705100c9ee68201afbed826be5
berkyvz <berkyavuz@gmail.com>
2019-11-05 11:33:34 +0300

    electron gui init

---
+++ gui/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.loadFile('index.html')
+  
+  win.on('closed', () => {
+    win = null
+  })  
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
