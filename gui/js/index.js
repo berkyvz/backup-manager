da32bafebb7d00dfa505fe5804171cc2a700a9fb
berkyvz <berkyavuz@gmail.com>
2019-11-08 15:25:15 +0300

    gui with electronJS and expressJS is completed

--- gui/js/index.js
+++ gui/js/index.js
@@ -1,45 +1,77 @@
-function updateFileList(){
-    var fileList = document.getElementById("file-list");
-    fileList.innerHTML = "Reading..."
-    
-    var html = "";
-    
-    setTimeout(function(){ 
-        fetch('http://localhost:3000').then(res =>  res.json()).then( data => {
-        for(elementIndex in data.folders){
-            //<li onclick="itemClicked(event);" class="list-group-item">Cras justo odio</li>
-            var element = `<li onclick="itemClicked(event);" item-index=${elementIndex} class="list-group-item">${data.folders[elementIndex]}</li>`;
-           html = html + element;
-        }
-    fileList.innerHTML = html;
-    });
-     }, 340);
+function progress(isActive) {
+    if (isActive) {
+        document.getElementById("load-renderer").innerHTML += '<div id="progress" class="loader-container"><div class="loader"></div></div>';
+    }
+    if (!isActive) {
+        let loading = document.getElementById("progress");
+        loading.remove();
+    }
 }
 
-function removeFromList(){
-    let selectedItem = document.getElementsByClassName("selected")[0];
-    selectedItem.remove();
-    let index = selectedItem.getAttribute("item-index");
-    let value = selectedItem.lastChild;
-    var obj = { 'index' : index,
-                'value' : value.data
-            };
-    console.log(obj);
-    fetch('http://localhost:3000/remove-from-list', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(obj)
-       }).then(res => res.json()).then( data =>  {
+
+function updateFileList() {
+
+    var fileList = document.getElementById("file-list");
+    progress(true);
+    fileList.innerHTML = "Reading..."
+    var html = "";
+
+    setTimeout(function () {
+        fetch('http://localhost:3000').then(res => res.json()).then(data => {
+
+            if (!data.folders[0] == "") {
+
+
+                for (elementIndex in data.folders) {
+                    var element = `<li onclick="itemClicked(event);" item-index=${elementIndex} class="list-group-item">${data.folders[elementIndex]}</li>`;
+                    html = html + element;
+                }
+                fileList.innerHTML = html;
+
+            }
+            else {
+                fileList.innerHTML = "Not Found. Add More."
+                document.getElementById("save-btn").setAttribute("disabled","disabled");
+            }
+            progress(false);
+        });
+    }, 340);
+}
+
+
+function removeFromList() {
+    try {
+        progress(true);
+        let selectedItem = document.getElementsByClassName("selected")[0];
+        selectedItem.remove();
+
+        let index = selectedItem.getAttribute("item-index");
+        let value = selectedItem.lastChild;
+        var obj = {
+            'index': index,
+            'value': value.data
+        };
+
+        fetch('http://localhost:3000/remove-from-list', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        }).then(res => res.json()).then(data => {
+            progress(false);
             alert(`Message: ${data.status}`);
         });
 
-    updateFileList();
+        updateFileList();
+    } catch (error) {
+        console.log("HATA OLUŞTU");
+    }
+
 }
 
-function itemClicked(e){
+function itemClicked(e) {
     var a = document.getElementsByTagName('li');
     for (i = 0; i < a.length; i++) {
         a[i].classList.remove('active')
@@ -51,25 +83,93 @@ function itemClicked(e){
 }
 
 
-function addToList(e){
+function addToList(e) {
     var element = document.getElementById("list-item");
 
-    var obj = {'path': element.value};
-    
-    fetch('http://localhost:3000/add-file', {
+    if (element.value === "") {
+        alert(`Message: This is not a correct path`);
+    } else {
+        var obj = { 'path': element.value };
+
+        fetch('http://localhost:3000/add-file', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        }).then(res => res.json()).then(data => {
+
+        });
+    }
+
+
+    updateFileList();
+    element.value = "";
+
+}
+
+
+function setInputEditable() {
+    let input = document.getElementById("destination-path-input");
+    input.removeAttribute("readonly");
+}
+
+function saveInput() {
+    progress(true);
+    let input = document.getElementById("destination-path-input");
+    obj = { "path": input.value }
+    fetch('http://localhost:3000/save-destination', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-          },
+        },
         body: JSON.stringify(obj)
-       }).then(res => res.json()).then( data =>  {
-        alert(`Message: ${data.status}`);
-        });
+    }).then(res => res.json()).then(data => {
+        progress(false);
+    });
 
-        updateFileList();
-        element.value = "";
-    
+
+    input.setAttribute("readonly", "readonly");
+}
+
+function updateFromFile() {
+    progress(true);
+    let input = document.getElementById("destination-path-input");
+
+
+    setTimeout(() => {
+        fetch('http://localhost:3000').then(res => res.json()).then(data => {
+
+            let path = data.destination;
+            input.setAttribute("placeholder", `${path}`);
+            input.value = `${path}`;
+            input.setAttribute("readonly", "readonly");
+            progress(false);
+        });
+    }, 300);
+}
+
+
+function runBackup() {
+
+    progress(true);
+
+    let inputPath = document.getElementById("destination-path-input").value;
+    var obj = { path: inputPath };
+    fetch('http://localhost:3000/backup', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    }).then(res => res.json()).then(data => {
+        progress(false);
+        alert(`Message: ${data.status}`);
+    });
 }
 
 updateFileList();
+updateFromFile();
--- gui/main.js
+++ gui/main.js
@@ -1,17 +1,19 @@
 const { app, BrowserWindow, Menu } = require('electron')
+const shell = require('shelljs')
 const express = require('express')
-const server = express()
-const port = 3000
-
 const util = require("util");
 const fs = require("fs");
+const server = express()
+
+
+const port = 3000
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 
 let win
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 900,
@@ -32,31 +34,53 @@ function createWindow () {
     });
   });
 
-  server.post('/remove-from-list' , (req , res) => {
+  server.post('/remove-from-list', (req, res) => {
     readSettings().then(settingArray => {
       settingArray.folders.splice(req.body.index, 1);
-      writeSettings(settingArray.folders.toString()).then(
-        res.send({'status': `${req.body.value} is removed.`})
+      writeSettings("../folder-list.settings", settingArray.folders.toString()).then(
+        res.send({ 'status': `${req.body.value} is removed.` })
       );
     });
   });
 
-  server.post('/add-file' , (req , res) => {
+  server.post('/add-file', (req, res) => {
     readSettings().then(settingArray => {
       const newPath = req.body.path.toString();
-      const newFileContent = `${settingArray.folders.toString()},${newPath}`.replace("\n","");
-      writeSettings(newFileContent).then(a => {
-        res.send({'status': `${req.body.path} is added.`})
+      var newFileContent = ""
+      if(`${settingArray.folders.toString()}`.length < 2){
+         newFileContent = `${newPath}`.replace("\n", "");
+      }else{
+         newFileContent = `${settingArray.folders.toString()},${newPath}`.replace("\n", "");
+      }
+      
+      writeSettings("../folder-list.settings", newFileContent).then(a => {
+        res.send({ 'status': `${req.body.path} is added.` })
       })
+    });
   });
-});
+
+
+  server.post('/save-destination', (req, res) => {
+    const newPath = req.body.path.toString();
+    writeSettings("../backup-destination.settings", newPath).then(a => {
+      res.send({ 'status': `${req.body.path} is changed.` })
+    });
+  });
+
+  server.post('/backup', (req, res) => {
+
+    shell.config.execPath = shell.which('node');
+    shell.exec('bash ../manager-gui.sh');
+    res.send({ 'status': "Backup Complete." });
+  });
+
 
   server.listen(port, () => console.log(`App is running on port ${port}!`))
 
 
   win.on('closed', () => {
     win = null
-  }); 
+  });
 }
 
 app.on('ready', createWindow);
@@ -72,22 +96,23 @@ app.on('activate', () => {
 });
 
 
-async function readSettings(){
+async function readSettings() {
 
- let fileList =  await readFile('../folder-list.settings', 'utf8');
- fileList = fileList.split(",");
- let backupDestination =  await readFile('../backup-destination.settings', 'utf8');
- backupDestination = backupDestination.split(",");
- 
- var obj = {
+  let fileList = await readFile('../folder-list.settings', 'utf8');
+  fileList = fileList.split(",");
+  let backupDestination = await readFile('../backup-destination.settings', 'utf8');
+  backupDestination = backupDestination.split(",");
+
+  var obj = {
     'folders': fileList,
     'destination': backupDestination
-  } 
+  }
   return obj;
 }
 
-async function writeSettings(content){
+
+async function writeSettings(path, content) {
   const writeResult = await writeFile(
-    "../folder-list.settings", content);
-   return true;
- }
+    `${path}`, content);
+  return true;
+}
